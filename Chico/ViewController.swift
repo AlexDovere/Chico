5dd1baeaebcade53dfd3f4cfee1ca3b9474cad83
Alessandro Dovere <a.dovere@quadronica.com>
2022-11-22 21:13:20 +0100

    Initial Commit

---
+++ Chico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chico
+//
+//  Created by Alessandro Dovere on 22/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
