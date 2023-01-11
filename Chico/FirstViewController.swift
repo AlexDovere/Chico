9d1818ed3dd00ea21e73442b3cc786e20fbbd4ca
Alessandro Dovere <a.dovere@quadronica.com>
2023-01-11 13:10:47 +0100

    Added xib logic

--- Chico/FirstViewController.swift
+++ Chico/FirstViewController.swift
@@ -7,17 +7,24 @@
 
 import UIKit
 
-class ViewControllerTest: UIViewController {
+class FirstViewController: UIViewController {
     @IBOutlet var viewBackground: UIView!
-    @IBOutlet weak var label: UILabel!
+//    @IBOutlet weak var label: UILabel!
     let network = DogBreedsApi()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         network.getBreeds()
-        label.text = "Test 1"
-        label.font = .systemFont(ofSize: 40)
     }
 }
 
